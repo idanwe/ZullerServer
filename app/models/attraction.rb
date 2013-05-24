68edb4f5b01f80caba60b51d57da29c0bf8d8869
idanw <idanwe2@gmail.com>
2013-05-24 17:38:54 +0300

    add created_at and contact_phone to attraction

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,8 +1,11 @@
 class Attraction
   include Mongoid::Document
+  include Mongoid::Timestamps
+  
   field :name, type: String
   field :address, type: String
   field :phone_number, type: String
+  field :contact_phone, type: String
   field :min_age, type: Integer
   field :logo, type: String
   field :music, type: Array
@@ -13,5 +16,5 @@ class Attraction
   # available_credits - embed array <Credit>
   # owner - embed <User>
 
-  # validates_presence_of :name, :min_age, :address, :date, :phone_number
+  validates_presence_of :name, :address
 end

11ad0a186709f80100432c1be13d1897d165cebd
idanw <idanwe2@gmail.com>
2013-06-05 16:53:39 +0300

    add musicPreferences as enumeraize

--- app/models/app_user.rb
+++ app/models/app_user.rb
@@ -1,12 +1,18 @@
+require 'music_enums'
+
 class AppUser
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+  extend Enumerize
+
   field :deviseIdentifier, type: String
   field :fbToken, type: String
   field :fbName, type: String
   field :age, type: Integer
-  field :musicPreferences, type: Array
+  
+  field :musicalPreferences, type: Array
+  enumerize :musicalPreferences, in: Music.for_select, multiple: true
+
   field :pastAttractions, type: Hash
 
   # field :location, type: GeoLocation

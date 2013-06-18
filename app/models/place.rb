ab0d40a26df5f915126c99e8b798b8009ed0ae96
idanw <idanwe2@gmail.com>
2013-06-18 23:35:12 +0300

    add lat lng and remove menu

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,11 +1,13 @@
 class Place < Attraction
   field :address, type: String
+  field :lat, type: Float
+  field :lng, type: Float
   field :opening_hours, type: Time #DateRange
   field :menu, type: String
   field :alcohol_prices, type: Hash
 
   has_many :parties, class_name: 'Party', inverse_of: :host
   has_many :lines, class_name: 'Line', inverse_of: :host
-  
-  validates_presence_of :address
+
+  validates_presence_of :address, :lat, :lng
 end

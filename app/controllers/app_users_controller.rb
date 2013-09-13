e6bb26df40188855acb78fc44435c150276c28a2
idanw <idanwe2@gmail.com>
2013-09-13 13:49:50 +0300

    create unique user for device_id and update it with fb_user_id

--- app/controllers/app_users_controller.rb
+++ app/controllers/app_users_controller.rb
@@ -40,7 +40,26 @@ class AppUsersController < ApplicationController
   # POST /app_users
   # POST /app_users.json
   def create
-    @app_user = AppUser.new(params[:app_user])
+    user_with_device_id = AppUser.where(device_id: params[:app_user][:device_id])
+    if user_with_device_id.exists?
+      @app_user = user_with_device_id.first
+    end
+
+    if params[:app_user][:fb_user_id].present?
+      user_with_fb_id = AppUser.where(fb_user_id: params[:app_user][:fb_user_id])
+      if user_with_fb_id.exists?
+        if @app_user.nil?
+          @app_user = user_with_fb_id.first
+        end
+        @app_user.update_attributes(params[:app_user])
+      elsif @app_user.present?
+        @app_user.update_attributes(params[:app_user])
+      end
+    end
+
+    if @app_user.nil?
+      @app_user = AppUser.new(params[:app_user])
+    end
 
     respond_to do |format|
       if @app_user.save
@@ -52,6 +71,58 @@ class AppUsersController < ApplicationController
       end
     end
   end
+
+  # def create
+  #   fb_user_id = params[:app_user][:fb_user_id]
+  #   debugger
+  #   if fb_user_id.present?
+  #     # if AppUser.fb_id_exists? fb_user_id
+  #     @app_user_with_fb_id = AppUser.where(fb_user_id: fb_user_id)
+  #     debugger
+  #     if @app_user_with_fb_id.exists?
+  #       @app_user = @app_user_with_fb_id.first
+  #       device_id = params[:app_user][:device_id]
+  #       debugger
+  #       if @app_user.devices_ids.nil?
+  #         @app_user.devices_ids = [device_id]
+  #         debugger
+  #       elsif not @app_user.devices_ids.include? device_id
+  #         debugger
+  #         @app_user.devices_ids << device_id
+  #         debugger
+  #       end
+  #     else
+  #       @app_user_with_device_id = AppUser.where(devices_ids: params[:app_user][:device_id])
+  #       debugger
+  #       if @app_user_with_device_id.exists?
+  #         @app_user = @app_user_with_device_id
+  #         params[:app_user][:devices_ids] << params[:app_user].delete(:device_id)
+  #         debugger
+  #         @app_user.update(params[:app_user])
+  #         @app_user = @app_user.first
+  #         debugger
+  #       end
+  #     end
+  #   end
+
+  #   debugger
+  #   if @app_user.nil?
+  #     params[:app_user][:devices_ids] = [params[:app_user].delete(:device_id)]
+  #     debugger
+  #     @app_user = AppUser.new(params[:app_user]) # not creating device_id as array
+  #   end
+
+  #   respond_to do |format|
+  #     debugger
+  #     if @app_user.save
+  #       format.html { redirect_to @app_user, notice: 'App user was successfully created.' }
+  #       format.json { render json: @app_user, status: :created, location: @app_user }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @app_user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PUT /app_users/1
   # PUT /app_users/1.json
